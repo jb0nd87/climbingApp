4da2b3b6478763a99801ec01580849477266b889
jb0nd87 <jbondoc87@gmail.com>
2020-12-04 08:12:03 -0800

    client file updated

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful';
 
 export const client = contentful.createClient({
-	space: process.env.REACT_APP_SPACE_ID,
-	accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+	space: 'boagj9la1x4o',
+	accessToken: 'gslGPsRqFucaPzYEBHYzW0eyjCX6H2DRmWLLKitpePY',
 });
