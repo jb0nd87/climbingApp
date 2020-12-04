0633b69ea600f86789684d5a1a045c900f398105
jb0nd87 <jbondoc87@gmail.com>
2020-12-03 18:52:32 -0800

    added components files

---
+++ src/Components/Climb/Climb.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Climb.scss';
+
+const Climb = ({ article }) => {
+	console.log(article);
+	const { name, description, image } = article.fields;
+	return (
+		<div className='post'>
+			<h2 className='title'>{name}</h2>
+			{image && (
+				<img
+					className='image'
+					src={image.fields.file.url}
+					alt={name}
+					title={name}
+				/>
+			)}
+			<section>{description}</section>
+		</div>
+	);
+};
+
+export default Climb;
