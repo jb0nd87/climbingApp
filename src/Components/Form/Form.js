493fbdde5f85d4a7e28c0ce5c2c0757b730ef990
jb0nd87 <jbondoc87@gmail.com>
2020-12-04 12:37:28 -0800

    form added

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './Form.scss';
+
+const Form = (props) => {
+	const [climbName, setClimbName] = useState('');
+
+	const handleSubmit = (event) => {
+		console.log('handleSubmit clicked');
+		event.preventDefault();
+		props.handleSubmit(climbName);
+		setClimbName('');
+	};
+
+	const handleChange = (event) => {
+		console.log('handleChange clicked');
+		const title = event.target.value;
+		setClimbName(name);
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='climbName'>Find a Route</label>
+				<input
+					id='name'
+					type='text'
+					value={climbName}
+					onChange={handleChange}
+				/>
+				<input type='submit' value='Submit' />
+			</form>
+		</>
+	);
+};
+
+export default Form;
